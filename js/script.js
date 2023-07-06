b56e327b1a3f8ae8c1235e925ed4e21779862017
rakaiskandar <iskandarraka4@gmail.com>
2023-07-06 11:49:25 +0700

    change functionality and style

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 const btnMeal = document.getElementById("get-meal");
 const mealItem = document.getElementById("meal");
 
-btnMeal.addEventListener("click", () => {
-  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(
-      res => {
-        createMeal(res.meals[0])
-      }
-    );
+const getApiRequest = async () => {
+  try {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+btnMeal.addEventListener("click", async () => {
+  try {
+    const meal = await getApiRequest();
+    createMeal(meal.meals[0]);
+
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 const createMeal = (meal) => {
@@ -23,31 +34,32 @@ const createMeal = (meal) => {
       break;
     }
   }
+
   const showMeal = `
-<div class="row">
-  <div class="columns five">
-    <img src="${meal.strMealThumb}" alt="Meal Image">
-    ${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
-    ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-    ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
-    <h5>Ingredients:</h5>
-    <ul>
-      ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-    </ul>
-  </div>
-  <div class="columns seven">
-    <h4>${meal.strMeal}</h4>
-    <p>${meal.strInstructions}</p>
-  </div>
-</div>
-${meal.strYoutube ? `
-<div class="row">
-  <h5>Video Recipe</h5>
-    <iframe width="420" height="315"
-    src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
-    </iframe>
-</div>` : ''}
-`;
+      <div class="row">
+        <div class="columns five">
+          <img src="${meal.strMealThumb}" alt="Meal Image">
+          ${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+          ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+          ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+          <h5>Ingredients:</h5>
+          <ul>
+            ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+          </ul>
+        </div>
+        <div class="columns seven">
+          <h4>${meal.strMeal}</h4>
+          <p>${meal.strInstructions}</p>
+        </div>
+      </div>
+      ${meal.strYoutube ? `
+      <div class="row">
+        <h5>Video Recipe</h5>
+          <iframe width="420" height="315"
+          src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+          </iframe>
+      </div>` : ''}
+      `;
   mealItem.innerHTML = showMeal;
 };
 
